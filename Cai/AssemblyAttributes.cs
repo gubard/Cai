3f3fede84313a1807e6dd311a940960b0e83c81e
gubard <maksimovsergey08@gmail.com>
2026-01-21 15:15:56 +0200

    Remove redundant AssemblyAttributes references.

--- Cai/AssemblyAttributes.cs
+++
@@ -1,8 +0,0 @@
-﻿using Cai.Ui;
-using Inanna.Generator;
-
-[assembly: ViewPair(typeof(FilesPanelView), typeof(FilesPanelViewModel))]
-[assembly: ViewPair(typeof(FilesView), typeof(FilesViewModel))]
-[assembly: ViewPair(typeof(FtpParametersView), typeof(FtpParametersViewModel))]
-[assembly: ViewPair(typeof(FtpFilesView), typeof(FtpFilesViewModel))]
-[assembly: ViewPair(typeof(FilesPanelHeaderView), typeof(FilesPanelHeaderViewModel))]
