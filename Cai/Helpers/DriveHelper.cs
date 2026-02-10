5f850a09ca71dba9ea1fbefd4496dbae09d87c1a
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-02-10 09:37:10 +0200

    Refactor: update `DriveHelper` to handle Linux drives with specific paths and icons based on `OsHelper.OsType`

--- Cai/Helpers/DriveHelper.cs
+++ Cai/Helpers/DriveHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Cai.Models;
+using Gaia.Helpers;
+using Gaia.Models;
+using IconPacks.Avalonia.MaterialDesign;
 
 namespace Cai.Helpers;
 
@@ -8,6 +11,32 @@ public static class DriveHelper
 
     static DriveHelper()
     {
-        Drives = DriveInfo.GetDrives().Select(x => new FileNotify(x)).ToArray();
+        switch (OsHelper.OsType)
+        {
+            case Os.Linux:
+                Drives = new FileNotify[]
+                {
+                    new("78AE0220-8BAE-4B92-BA77-2FF48CD9762B".ToGuid())
+                    {
+                        Name = "/",
+                        Path = "/",
+                        IsFrozen = true,
+                        Icon = PackIconMaterialDesignKind.Folder,
+                    },
+                    new("70C36E25-7F0B-4686-B0F0-4E91933D1CC6".ToGuid())
+                    {
+                        Name = Environment.UserName,
+                        Path = $"/home/{Environment.UserName}",
+                        IsFrozen = true,
+                        Icon = PackIconMaterialDesignKind.Folder,
+                    },
+                };
+
+                break;
+            default:
+                Drives = DriveInfo.GetDrives().Select(x => new FileNotify(x)).ToArray();
+
+                break;
+        }
     }
 }
